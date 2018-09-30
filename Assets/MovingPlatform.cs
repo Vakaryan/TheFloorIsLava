7eee43674928e7612e7e57278315d66e90902f97
LoW <lea-saunier@hotmail.fr>
2018-09-30 14:52:46 +0200

    Unity project creation. Level design first look

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+    public float range;
+    public enum MovementType { horizontal, vertical };
+    public MovementType curMove;
+    private Vector2 startingPosition;
+    public float startingDirection;
+    public float speed;
+
+
+
+	// Use this for initialization
+	void Start () {
+        startingPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (curMove)
+        {
+            case MovementType.horizontal:
+                transform.position += new Vector3(speed * startingDirection * Time.deltaTime, 0f, 0f);
+                if (startingDirection == 1 && transform.position.x >= startingPosition.x + range)
+                {
+                    startingDirection *= -1;
+                    startingPosition.x += range;
+                }
+                if (startingDirection == -1 && transform.position.x <= startingPosition.x - range)
+                {
+                    startingDirection *= -1;
+                    startingPosition.x -= range;
+                }
+                break;
+
+            case MovementType.vertical:
+                transform.position += new Vector3(0f, speed * startingDirection * Time.deltaTime, 0f);
+                if (startingDirection == 1 && transform.position.y >= startingPosition.y + range)
+                {
+                    startingDirection *= -1;
+                    startingPosition.y += range;
+                }
+                if (startingDirection == -1 && transform.position.y <= startingPosition.y - range)
+                {
+                    startingDirection *= -1;
+                    startingPosition.y -= range;
+                }
+                break;
+        }
+	}
+}
---
+++ Assets/TheGroundIsLava.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheGroundIsLava : MonoBehaviour {
+    public GameObject startingPoint;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            col.transform.position = startingPoint.transform.position;
+        }
+    }
+}
