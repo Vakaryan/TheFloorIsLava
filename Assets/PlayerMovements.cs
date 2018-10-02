21fadeefcccd109643d069027fb2eff7bdec0313
LoW <lea-saunier@hotmail.fr>
2018-10-02 16:56:23 +0200

    Movements ok, dash middly ok

--- Assets/PlayerMovements.cs
+++ Assets/PlayerMovements.cs
@@ -28,9 +28,13 @@ public class PlayerMovements : MonoBehaviour {
             transform.position += new Vector3(Input.GetAxis("Horizontal") * speed, 0, 0);
 
         }
-        if (Input.GetButtonDown("Dash") && timerDash <= 0)
+        else
         {
             Dash();
+        }
+        if (Input.GetButtonDown("Dash") && timerDash <= 0)
+        {
+            dashing = true;
         }
 	}
 
@@ -50,14 +54,13 @@ public class PlayerMovements : MonoBehaviour {
 
     private void Dash() { 
         Debug.Log("Dashing");
-        dashing = true;
-        timerDash = 0;
         if (oldPos < transform.position.x) //player moving to the right
         {
             transform.position += new Vector3(dashSpeed * Time.deltaTime, 0, 0);
             if (transform.position.x - oldPos >= dashDist)
             {
                 oldPos = transform.position.x;
+                dashing = false;
             }
         }
         else if (oldPos > transform.position.x) //player moving to the left
@@ -66,6 +69,7 @@ public class PlayerMovements : MonoBehaviour {
             if (oldPos - transform.position.x >= dashDist)
             {
                 oldPos = transform.position.x;
+                dashing = false;
             }
         }
         else //player not moving, using old dir to choose
@@ -76,6 +80,7 @@ public class PlayerMovements : MonoBehaviour {
                 if (transform.position.x - oldPos >= dashDist)
                 {
                     oldPos = transform.position.x;
+                    dashing = false;
                 }
             }
             if (dir == -1)
@@ -84,11 +89,11 @@ public class PlayerMovements : MonoBehaviour {
                 if (oldPos - transform.position.x >= dashDist)
                 {
                     oldPos = transform.position.x;
+                    dashing = false;
                 }
             }
         }
-        timerDash = resetTimer;
-        dashing = false;
+        //timerDash = resetTimer;
     }
 
 }
