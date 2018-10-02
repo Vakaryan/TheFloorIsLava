a6fea99176cf2ea61c4ad20a4b7a7cb0d0797d8c
Unknown <mockingspark@gmail.com>
2018-10-02 14:57:09 +0200

    GravityFall
    Gravity and jump OK

--- Assets/Gravity.cs
+++ Assets/Gravity.cs
@@ -4,35 +4,24 @@ using UnityEngine;
 
 public class Gravity : MonoBehaviour {
 
-    private Collider2D playerCollider;
-
-    public float verticalSpeed = 0;
-
-    private Ray2D verticalRay;
 
 
-    // Use this for initialization
-    void Start () {
-        playerCollider = GetComponent<Collider2D>();
-        verticalRay = new Ray2D();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private VerticalCollide vertPhysics;
 
-        verticalRay.direction = new Vector2(-1, 0);
-        verticalRay.origin = playerCollider.bounds.min + new Vector3(-1, 0, 0);
+    public float gravityAcceleration;
 
-        RaycastHit2D hit = Physics2D.Raycast(verticalRay.origin, verticalRay.direction, verticalSpeed +1f);
-        
-        if (hit.transform)
-        {
-            Debug.Log(hit.collider);
-        }
-        else
-        {
-            Debug.Log("Raycast hit nothing");
-        }
+    private void Start()
+    {
+        vertPhysics = GetComponent<VerticalCollide>(); 
+    }
 
-	}
+    // Update is called once per frame
+    void Update () {
+
+        float speed = vertPhysics.VerticalSpeed;
+        speed -= gravityAcceleration;
+        vertPhysics.VerticalSpeed = speed;
+
+
+    }
 }
---
+++ Assets/Jump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+    private VerticalCollide vertPhysics;
+
+    public string button;
+    public float impulseForce;
+
+    private void Start()
+    {
+        vertPhysics = GetComponent<VerticalCollide>();
+    }
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetButtonDown(button))
+        {
+            vertPhysics.VerticalSpeed += impulseForce;
+        }
+	}
+}
