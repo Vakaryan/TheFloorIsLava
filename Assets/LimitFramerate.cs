4a4721659425bb554ed96d3e2024415419db4269
LoW <lea-saunier@hotmail.fr>
2018-10-27 17:25:38 +0200

    Framerate limited to 60

---
+++ Assets/LimitFramerate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitFramerate : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+	}
+	
+}
