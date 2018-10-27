f7ab0dea7b41a1b2f031120c75b7181867530018
LoW <lea-saunier@hotmail.fr>
2018-10-27 17:31:10 +0200

    Limit framerate to custom fps

--- Assets/LimitFramerate.cs
+++ Assets/LimitFramerate.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LimitFramerate : MonoBehaviour {
+    public int fps;
 
 	// Use this for initialization
 	void Awake () {
         QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 60;
+        Application.targetFrameRate = fps;
 	}
 	
 }
